1742070a2d179ee413b2014ae4931df0e26ae4f1
Riju Ghosh <pg4009355@gmail.com>
2025-02-25 19:17:33 +0530

    On Going

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -8,7 +8,7 @@ export default function LandingPage() {
     <div className="landingPageContainer">
       <nav>
         <div className="navHeader">
-          <h2>Apna Video Call</h2>
+          <h2>Enjoy Your Video Call</h2>
         </div>
         <div className="navlist">
           <p
