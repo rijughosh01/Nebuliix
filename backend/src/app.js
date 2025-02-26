bfe69a3f8df9a5ce59397358efa447e034f0354a
Riju Ghosh <pg4009355@gmail.com>
2025-02-26 18:22:38 +0530

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 
@@ -13,8 +10,14 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+const corsOptions = {
+  origin: ["https://nebuliix.vercel.app", "http://localhost:3000"], // Add your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 app.set("port", process.env.PORT || 8000);
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
@@ -25,9 +28,10 @@ const start = async () => {
   const connectionDb = await mongoose.connect(
     "mongodb+srv://pg4009355:TM55VDc9C2kJtZJ8@zerodhaclonecluster.bycub.mongodb.net/?retryWrites=true&w=majority&appName=ZerodhaCloneCluster"
   );
-  console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`);
+
+  console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
   server.listen(app.get("port"), () => {
-    console.log("Server is running on port 8000");
+    console.log(`LISTENING ON PORT ${app.get("port")}`);
   });
 };
 
