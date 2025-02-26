b67aaae506f09314b3a597c4f559fdd1d0ad5c58
Riju Ghosh <pg4009355@gmail.com>
2025-02-26 11:29:44 +0530

    then

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import server from "../environment";
+// import server from "../environment";
 
 export const AuthContext = createContext({});
 
 const client = axios.create({
-  baseURL: `${server}/api/v1/users`,
+  baseURL:"https://nebuliix-backend.vercel.app/",
 });
 
 export const AuthProvider = ({ children }) => {
