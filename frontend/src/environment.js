843a6f41cb0e749a3d68a37957940d51ccb6a0e6
Pritam Ghosh <pg4009355@gmail.com>
2025-02-26 17:44:06 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://nebuliix-backend.vercel.app"
-  : "http://localhost:8000";
+  ? "http://localhost:8000"
+  : "https://nebuliix-backend.vercel.app";
 
 export default server;
